110687b56d43ba90edad44ceafd8e4b9c336829c
80chucho08 <chucho2004carbajal@gmail.com>
2025-10-30 12:52:26 -0600

    eliminacion de archivos innecesarios

--- mercadolibre/paginas/cerrarsesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    session_unset();     // limpia variables de sesión
-    session_destroy();   // destruye la sesión	
-
-    echo "<script language='javascript'>alert('Sesion cerrada exitosamente'); document.location.href='inicio.php?op=acceso';</script>";
-
-?>
